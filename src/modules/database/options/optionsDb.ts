46f85f5376029a7437850ff1f4eb52ca77777e65
magraina <marinafcegner@gmail.com>
2024-02-17 19:51:27 +0100

    feat: Added more routes

--- src/modules/database/options/optionsDb.ts
+++ src/modules/database/options/optionsDb.ts
@@ -6,17 +6,11 @@ import { getSurveyIdFromDb } from '../survey/surveyDb';
 import { keysToCamelCase } from '../../misc/convertToCamel';
 import { z as zod } from 'zod';
 
-
-const getOption = zod.object({
-	content: zod.string(),
-	optionId: zod.string().uuid(),
-	optionName: zod.string(),
-});
-
+/** Adds a new option to the survey into the DB */
 const addOptionToDb = async (creationToken: string, optionName: string, content: string) => {
 	const conn = await getConnection();
 	const optionId = crypto.randomUUID();
-	const surveyId = getSurveyIdFromDb(creationToken);
+	const surveyId = await getSurveyIdFromDb(creationToken);
 	await conn.query(`INSERT INTO options
 	(survey_id, option_name, content, option_id)
 	VALUES (?, ?, ?, ?, ?, ?)`, [
@@ -25,9 +19,30 @@ const addOptionToDb = async (creationToken: string, optionName: string, content:
 		content,
 		optionId,
 	]);
+
 	return optionId;
 };
 
+/** Updates a option to the survey into the DB */
+const updateOptionToDb = async(creationToken: string, optionId: string, optionName: string, content: string) => {
+	const conn = await getConnection();
+	const surveyId = await getSurveyIdFromDb(creationToken);
+	await conn.query(`UPDATE options SET option_name = ?, content = ?
+	WHERE survey_id=? AND option_id=?`, [
+		optionName,
+		content,
+		surveyId,
+		optionId,
+	]);
+};
+
+const getOption = zod.array(zod.object({
+	content: zod.string(),
+	optionId: zod.string().uuid(),
+	optionName: zod.string(),
+}));
+
+/** Returns a option from the survey in the DB */
 const getOptionFromDb = async (creationToken: string, optionId: string) => {
 	const conn = await getConnection();
 	const response = await conn.query(`SELECT option_id, option_name, content
@@ -38,11 +53,26 @@ const getOptionFromDb = async (creationToken: string, optionId: string) => {
 		creationToken,
 		optionId,
 	]);
-	const converted = keysToCamelCase(response[0]);
+
+	const converted = keysToCamelCase(response);
+	const validated = getOption.parse(converted);
+	return validated[0];
+};
+
+/** Returns all option from the survey in the DB */
+const getAllOptionFromDb = async(creationToken: string) => {
+	const conn = await getConnection();
+	const response = await conn.query(`SELECT option_id, option_name, content
+	FROM options
+	JOIN survey ON options.survey_id = survey.survey_id
+	WHERE creation_token = ?`, [creationToken]);
+
+	const converted = keysToCamelCase(response);
 	const validated = getOption.parse(converted);
 	return validated;
 };
 
+/** Removes all options from the survey in the DB */
 const removeOptionsFromDb = async (creationToken: string) => {
 	const conn = await getConnection();
 	await conn.query(`DELETE FROM options 
@@ -50,4 +80,23 @@ const removeOptionsFromDb = async (creationToken: string) => {
 	WHERE creation_token = ?)`, [creationToken]);
 };
 
-export { addOptionToDb, getOptionFromDb, removeOptionsFromDb };
+/** Removes a option from the survey in the DB */
+const removeOptionFromDb = async (creationToken: string, optionId: string) => {
+	const conn = await getConnection();
+	await conn.query(`DELETE FROM options 
+	WHERE option_id = ?
+	AND survey_id = (SELECT survey_id FROM survey
+	WHERE creation_token = ?)`, [
+		optionId,
+		creationToken,
+	]);
+};
+
+export {
+	addOptionToDb,
+	getAllOptionFromDb,
+	getOptionFromDb,
+	removeOptionFromDb,
+	removeOptionsFromDb,
+	updateOptionToDb,
+};
--- src/modules/routes/optionsRoutes.ts
+++ src/modules/routes/optionsRoutes.ts
@@ -1,8 +1,7 @@
-
+import { addOptionToDb, getAllOptionFromDb, getOptionFromDb, removeOptionFromDb, updateOptionToDb } from '../database/options/optionsDb';
+import { handleCreationResponse, handleSuccessResponse } from '../handler/handleSuccessResponse';
 import express from 'express';
-import { addOptionToDb, getOptionFromDb } from '../database/options/optionsDb';
 import { handleErrorResponse } from '../handler/handleErrorResponse';
-import { handleSuccessResponse } from '../handler/handleSuccessResponse';
 import { z as zod } from 'zod';
 
 
@@ -29,24 +28,81 @@ router.get('/getOption', async (req, res) => {
 });
 
 /**
- * Add a new option for this survey to DB
+ * Gets All options for this survey from DB
  */
 
-const addOptionRequest = zod.object({
+const getAllOptionParams = zod.object({
 	creationToken: zod.string().regex(/^[A-Za-z0-9+/]*/),
-	optionName: zod.string(),
-	content: zod.string(),
 });
 
-router.post('/addOption', async (req, res) => {
+router.get('/getAllOptions', async (req, res) => {
 	try {
-		const { content, creationToken, optionName } = addOptionRequest.parse(req.body);
-		const optionId = await addOptionToDb(creationToken, optionName, content);
-		handleSuccessResponse(req, res, { optionId });
+		const { creationToken } = getAllOptionParams.parse(req.params);
+		const options = await getAllOptionFromDb(creationToken);
+		handleSuccessResponse(req, res, options);
 	} catch (error) {
 		handleErrorResponse(req, res, error);
 	}
 });
 
+/**
+ * Add a new option for this survey to DB
+ */
+
+const addOptionRequest = zod.object({
+	content: zod.string(),
+	creationToken: zod.string().regex(/^[A-Za-z0-9+/]*/),
+	optionName: zod.string(),
+});
+
+router.post('/addOption', async (req, res) => {
+	try {
+		const { content, creationToken, optionName } = addOptionRequest.parse(req.body);
+
+		// TODO: Content should be checked with domify
+		const optionId = await addOptionToDb(creationToken, optionName, content);
+		handleCreationResponse(req, res, { optionId });
+	} catch (error) {
+		handleErrorResponse(req, res, error);
+	}
+});
+
+/**
+ * Update an existing option
+ */
+
+const updateOptionRequest = zod.object({
+	content: zod.string(),
+	creationToken: zod.string().regex(/^[A-Za-z0-9+/]*/),
+	optionId: zod.string().uuid(),
+	optionName: zod.string(),
+});
+
+router.post('/addOption', async (req, res) => {
+	try {
+		const { content, creationToken, optionId, optionName } = updateOptionRequest.parse(req.body);
+
+		// TODO: Content should be checked with domify
+		await updateOptionToDb(creationToken, optionId, optionName, content);
+		handleSuccessResponse(req, res, {});
+	} catch (error) {
+		handleErrorResponse(req, res, error);
+	}
+});
+
+/**
+ * Removes a existing option
+ */
+
+router.post('/deleteOption', async (req, res) => {
+	try {
+		const { creationToken, optionId } = getOptionParams.parse(req.body);
+
+		await removeOptionFromDb(creationToken, optionId);
+		handleSuccessResponse(req, res, {});
+	} catch (error) {
+		handleErrorResponse(req, res, error);
+	}
+});
 
 export { router as optionsRoutes };
