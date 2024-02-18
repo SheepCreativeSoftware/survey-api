e2225365215ab1323ca91823853f97a9c2a4dd35
magraina <marinafcegner@gmail.com>
2024-02-18 14:16:31 +0100

    feat: Exit on uncaught exception

--- src/app.ts
+++ src/app.ts
@@ -13,4 +13,16 @@ const startup = async () => {
 	startServer();
 };
 
+// eslint-disable-next-line no-shadow
+process.on('uncaughtException', (err, origin) => {
+	// Print last output
+	buntstift.error(`Caught exception: ${err}\n`);
+	buntstift.error(`Exception origin: ${origin}`);
+	if(err.stack) buntstift.error(`Stack: ${err.stack}`);
+	buntstift.error('Exiting Process...');
+
+	// Kill app, because you don't know what the concesquences will be (restart-on-failure from server-side)
+	process.exit(1);
+});
+
 startup();
